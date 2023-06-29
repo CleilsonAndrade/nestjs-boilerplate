39183b253f03fff4fc641f22355b48c7e63b8f0b
Cleilson-Andrade <cleilson.andrade@anjunexpress.com.br>
2023-06-29 11:00:34 -0300

    chore(component): fix config global
    add global value to acess env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule.forRoot({ isGlobal: true }),
     HealthModule,
     UserModule,
