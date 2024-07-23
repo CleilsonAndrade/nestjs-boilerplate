9d62ec7af5f3d4d9ae11d612e429adf517f1c443
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-07-22 23:25:12 -0300

    terter

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -5,8 +5,8 @@ import { AppService } from './app.service';
 @ApiTags('Default')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  constructor(private readonly appService: AppService) { }
+  sdfdsfsdf
   @Get()
   getHello(): string {
     return this.appService.getHello();
