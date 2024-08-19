f3b37a4699dfffcb8dc3736d831c38c231ac6380
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-08-19 12:31:23 -0300

    style: removed semi-collum

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 @ApiTags('Default')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
